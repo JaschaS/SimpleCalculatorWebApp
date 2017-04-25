dca25865fbfcd20ffa6defbf655a05329130b662
JaschaS <Jascha@jaschas-mbp.fritz.box>
2017-04-25 13:08:31 +0200

    It is now possible to add a digit to the number in the calculator.

--- src/java/de/jscholz/simplecalc/CalculatorHandler.java
+++ src/java/de/jscholz/simplecalc/CalculatorHandler.java
@@ -14,11 +14,26 @@ public class CalculatorHandler {
 
     @EJB
     private SimpleCalculator calculator;
+    private String displayText;
 
     public CalculatorHandler () {
+        displayText = "0";
     }
 
-    public void addDigit ( final String digit ) {
+    public void addDigit ( final int digit ) {
+        
+        //the digit in the range of 0 and 9 (both included).
+        //if the digit is less than 0 or greater than 9, return.
+        if(digit < 0 || 9 < digit) return;
+        
+        //We display always a zero, even if we don't diplay a number.
+        if(displayText.equals ( "0" )) {
+            
+            //Remove the zero, so that the display text is not e.g 01.
+            displayText = "";
+        }
+        
+        displayText += digit;
     }
 
     public void clear () {
@@ -42,6 +57,6 @@ public class CalculatorHandler {
 
     @Override
     public String toString () {
-        return "0";
+        return displayText;
     }
 }
