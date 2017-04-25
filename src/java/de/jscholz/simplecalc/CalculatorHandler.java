4a81b126e351eb12e4bbedb1349efb4029e74849
JaschaS <Jascha@jaschas-mbp.fritz.box>
2017-04-25 13:12:13 +0200

    It is now possible to remove the last digit and clear the display.

--- src/java/de/jscholz/simplecalc/CalculatorHandler.java
+++ src/java/de/jscholz/simplecalc/CalculatorHandler.java
@@ -21,25 +21,37 @@ public class CalculatorHandler {
     }
 
     public void addDigit ( final int digit ) {
-        
+
         //the digit in the range of 0 and 9 (both included).
         //if the digit is less than 0 or greater than 9, return.
-        if(digit < 0 || 9 < digit) return;
-        
+        if ( digit < 0 || 9 < digit ) {
+            return;
+        }
+
         //We display always a zero, even if we don't diplay a number.
-        if(displayText.equals ( "0" )) {
-            
+        if ( displayText.equals ( "0" ) ) {
+
             //Remove the zero, so that the display text is not e.g 01.
             displayText = "";
         }
-        
+
         displayText += digit;
     }
 
     public void clear () {
+
+        //If there is only one digit left, replace the digit with a zero.
+        if ( displayText.length () == 1 ) {
+            displayText = "0";
+        }
+        else {
+            //Remove the last added digit from the display.
+            displayText = displayText.substring ( 0, displayText.length () - 1 );
+        }
     }
 
     public void clearAll () {
+        displayText = "0";
     }
 
     public void subtract () {
