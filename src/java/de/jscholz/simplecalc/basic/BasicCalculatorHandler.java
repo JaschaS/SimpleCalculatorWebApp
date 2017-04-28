114671378bb790b5572f8f051ff59ccd1c96d296
JaschaS <Jascha@jaschas-mbp.fritz.box>
2017-04-28 18:10:06 +0200

    Postfix update

--- src/java/de/jscholz/simplecalc/basic/BasicCalculatorHandler.java
+++ src/java/de/jscholz/simplecalc/basic/BasicCalculatorHandler.java
@@ -1,7 +1,9 @@
 
 package de.jscholz.simplecalc.basic;
 
+import de.jscholz.simplecalc.PostFixCalculator;
 import de.jscholz.simplecalc.WebCalculator;
+import javax.ejb.EJB;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
@@ -13,44 +15,110 @@ import javax.faces.bean.ManagedBean;
 @ApplicationScoped
 public class BasicCalculatorHandler implements WebCalculator {
 
-    @Override
-    public void addDigit ( int digit ) {
-        throw new UnsupportedOperationException ( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
+    @EJB
+    private PostFixCalculator calculator;
+    private String displayText;
+    private String expression;
+    private String operator;
+
+    public BasicCalculatorHandler () {
+        displayText = "0";
+        operator = null;
     }
 
     @Override
-    public void addition () {
-        throw new UnsupportedOperationException ( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
-    }
+    public void addDigit ( final int digit ) {
 
-    @Override
-    public void subtract () {
-        throw new UnsupportedOperationException ( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
-    }
+        //the digit in the range of 0 and 9 (both included).
+        //if the digit is less than 0 or greater than 9, return.
+        if ( digit < 0 || 9 < digit ) {
+            return;
+        }
 
-    @Override
-    public void multiply () {
-        throw new UnsupportedOperationException ( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
-    }
+        if ( operator == null ) {
+            //We display always a zero, even if we don't diplay a number.
+            if ( displayText.equals ( "0" ) ) {
 
-    @Override
-    public void divide () {
-        throw new UnsupportedOperationException ( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
+                //Remove the zero, so that the display text is not e.g 01.
+                displayText = "";
+            }
+
+            displayText += digit;
+        }
+        else {
+
+            if ( displayText.contains ( "-" ) ) {
+                expression = "um" + displayText.substring ( 1, displayText.length () ) + " " + operator + " ";
+            }
+            else {
+                expression = displayText + " " + operator + " ";
+            }
+
+            displayText = digit + "";
+            operator = null;
+        }
     }
 
     @Override
     public void clear () {
-        throw new UnsupportedOperationException ( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
+
+        //If there is only one digit left, replace the digit with a zero.
+        if ( displayText.length () == 1 ) {
+            displayText = "0";
+        }
+        else {
+            //Remove the last added digit from the display.
+            displayText = displayText.substring ( 0, displayText.length () - 1 );
+        }
     }
 
     @Override
     public void clearAll () {
-        throw new UnsupportedOperationException ( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
+        displayText = "0";
+    }
+
+    @Override
+    public void subtract () {
+
+        //Otherwise set the operator.
+        if ( expression != null && !expression.isEmpty () ) {
+            result ();
+        }
+        operator = "-";
+
+    }
+
+    @Override
+    public void addition () {
+        if ( expression != null && !expression.isEmpty () ) {
+            result ();
+        }
+        operator = "+";
+    }
+
+    @Override
+    public void multiply () {
+        if ( expression != null && !expression.isEmpty () ) {
+            result ();
+        }
+        operator = "*";
+    }
+
+    @Override
+    public void divide () {
     }
 
     @Override
     public void result () {
-        throw new UnsupportedOperationException ( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
+        expression += displayText;
+        System.out.println ( expression );
+        displayText = calculator.calculate ( expression );
+        operator = null;
+        expression = null;
     }
-    
+
+    @Override
+    public String toString () {
+        return displayText;
+    }
 }
--- src/java/de/jscholz/simplecalc/postfix/PostifxCalculatorHandler.java
+++ src/java/de/jscholz/simplecalc/postfix/PostifxCalculatorHandler.java
@@ -1,7 +1,6 @@
 package de.jscholz.simplecalc.postfix;
 
 import de.jscholz.simplecalc.PostFixCalculator;
-import de.jscholz.simplecalc.WebCalculator;
 import javax.ejb.EJB;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
@@ -12,112 +11,29 @@ import javax.faces.bean.ManagedBean;
  */
 @ManagedBean
 @ApplicationScoped
-public class PostifxCalculatorHandler implements WebCalculator {
+public class PostifxCalculatorHandler {
 
     @EJB
     private PostFixCalculator calculator;
     private String displayText;
-    private String expression;
-    private String operator;
-
-    public PostifxCalculatorHandler () {
-        displayText = "0";
-        operator = null;
+    private String result;
+    
+    public void calculate() {
+        
+        if(displayText == null || displayText.isEmpty ()) return;
+        
+        result = calculator.calculate ( displayText );
     }
-
-    @Override
-    public void addDigit ( final int digit ) {
-
-        //the digit in the range of 0 and 9 (both included).
-        //if the digit is less than 0 or greater than 9, return.
-        if ( digit < 0 || 9 < digit ) {
-            return;
-        }
-
-        if ( operator == null ) {
-            //We display always a zero, even if we don't diplay a number.
-            if ( displayText.equals ( "0" ) ) {
-
-                //Remove the zero, so that the display text is not e.g 01.
-                displayText = "";
-            }
-
-            displayText += digit;
-        }
-        else {
-
-            if ( displayText.contains ( "-" ) ) {
-                expression = "um" + displayText.substring ( 1, displayText.length () ) + " " + operator + " ";
-            }
-            else {
-                expression = displayText + " " + operator + " ";
-            }
-
-            displayText = digit + "";
-            operator = null;
-        }
+    
+    public void setInput(final String input) {
+        displayText = input;
     }
-
-    @Override
-    public void clear () {
-
-        //If there is only one digit left, replace the digit with a zero.
-        if ( displayText.length () == 1 ) {
-            displayText = "0";
-        }
-        else {
-            //Remove the last added digit from the display.
-            displayText = displayText.substring ( 0, displayText.length () - 1 );
-        }
-    }
-
-    @Override
-    public void clearAll () {
-        displayText = "0";
-    }
-
-    @Override
-    public void subtract () {
-
-        //Otherwise set the operator.
-        if ( expression != null && !expression.isEmpty () ) {
-            result ();
-        }
-        operator = "-";
-
-    }
-
-    @Override
-    public void addition () {
-        if ( expression != null && !expression.isEmpty () ) {
-            result ();
-        }
-        operator = "+";
-    }
-
-    @Override
-    public void multiply () {
-        if ( expression != null && !expression.isEmpty () ) {
-            result ();
-        }
-        operator = "*";
-    }
-
-    @Override
-    public void divide () {
-    }
-
-    @Override
-    public void result () {
-        expression += displayText;
-        System.out.println ( expression );
-        displayText = calculator.calculate ( expression );
-        operator = null;
-        expression = null;
-    }
-
-    @Override
-    public String toString () {
+    
+    public String getInput() {
         return displayText;
+    }
+    
+    public String getResult() {
+        return result;
     }
 }
